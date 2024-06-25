369cbc786687a25febc0bca547c2e2727e7e4db0
Lê Trần Diễm Ly <147296100+diemly26@users.noreply.github.com>
2024-06-25 12:25:27 +0700

    Add files via upload

---
+++ DangNhap.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace dotnetProject
+{
+    partial class DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lb1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.Location = new System.Drawing.Point(764, 68);
+            this.lb1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(464, 76);
+            this.lb1.TabIndex = 2;
+            this.lb1.Text = "Đăng nhập";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lb1.UseMnemonic = false;
+            this.lb1.Click += new System.EventHandler(this.lb1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txt2);
+            this.panel1.Controls.Add(this.txt1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lb2);
+            this.panel1.Controls.Add(this.lb1);
+            this.panel1.Location = new System.Drawing.Point(48, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2000, 1200);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(864, 460);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Đăng nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(785, 319);
+            this.txt2.Name = "txt2";
+            this.txt2.PasswordChar = '*';
+            this.txt2.Size = new System.Drawing.Size(443, 31);
+            this.txt2.TabIndex = 1;
+            this.txt2.UseSystemPasswordChar = true;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(785, 255);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(443, 31);
+            this.txt1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(524, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mật khẩu:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb2
+            // 
+            this.lb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb2.Location = new System.Drawing.Point(507, 243);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(265, 48);
+            this.lb2.TabIndex = 3;
+            this.lb2.Text = "Tên đăng nhập:";
+            this.lb2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2474, 1429);
+            this.Controls.Add(this.panel1);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button button1;
+    }
+ 
+}
